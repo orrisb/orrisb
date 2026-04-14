4e21e32dc300468bab9b31caa72037d318dbdbb9
orris baker <orrisb@gmail.com>
2026-04-14 02:26:25 -0400

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// making sure the page is ready 
+
+$(document).ready(function() {
+   alert('Welcome to StarTrackr! Now no longer under police investigation!');
+  alert)$('#celebs tbody tr:even').length + ' elements!');
+});
